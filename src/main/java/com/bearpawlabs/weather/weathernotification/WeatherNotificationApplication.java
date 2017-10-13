7c9600039527fc4025c8d9768596206080890dd6
Andrew Vaughan <avaughan@afilias.info>
2017-10-13 17:16:11 +0100

    Adding structure and test file

---
+++ src/main/java/com/bearpawlabs/weather/weathernotification/WeatherNotificationApplication.java
@@ -0,0 +1,12 @@
+package com.bearpawlabs.weather.weathernotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherNotificationApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bearpawlabs/weather/weathernotification/WeatherNotificationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bearpawlabs.weather.weathernotification;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeatherNotificationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
